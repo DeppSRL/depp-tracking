86b1e6edd073200080afb618b444ed0f1407fe53
Guglielmo Celata <guglielmo.celata@gmail.com>
2015-06-12 17:13:27 +0200

    conflict solved after merge

--- project/tracking/models.py
+++ project/tracking/models.py
@@ -132,18 +132,12 @@ class BaseActivity(models.Model):
     )
 
     project = models.ForeignKey(Project, verbose_name=_("project"))
-<<<<<<< HEAD
-    activity_type = models.IntegerField(_("activity type"), choices=TYPES, null=True, blank=True, help_text=_("Select the type of activity. Don't be picky."))
-    description = models.CharField(_("description"), max_length=256, blank=True, null=True, help_text=_("A very brief description of the activity (max 256 chars)."))
-    hours = models.DecimalField(_('hours worked'), max_digits=3, decimal_places=1, help_text=_("Number of hours worked, can be a decimal"))
-=======
     activity_type = models.IntegerField(_("activity type"), choices=TYPES, null=True, blank=True,
                                         help_text=_("Select the type of activity. Don't be picky."))
     description = models.CharField(_("description"), max_length=256,
                                    help_text=_("A very brief description of the activity (max 256 chars)."))
     hours = models.DecimalField(_('hours worked'), max_digits=3, decimal_places=1,
                                 help_text=_("Number of hours worked, can be a decimal"))
->>>>>>> 27dde5ae8a76351d0a462383d16fbade605ca98a
 
     def __unicode__(self):
         return u"{0}".format(self.description)
